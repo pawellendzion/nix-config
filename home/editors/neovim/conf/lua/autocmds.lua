4efb0d684b5f11bb418c48dcf6a372e776ca09a8
Paweł Lendzion <pawellendzion01@gmail.com>
2024-03-19 20:49:57 +0100

    Delete neovim-config submodule

--- home/editors/neovim/conf/lua/autocmds.lua
+++
@@ -1,8 +0,0 @@
--- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-	group = vim.api.nvim_create_augroup('YankHighlight', { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-})

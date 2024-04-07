c3e67ef508c0532b0b7907f8101d125ab5870f02
Paweł Lendzion <pawellendzion01@gmail.com>
2024-04-07 14:19:16 +0200

    Add nix-direnv and configure shells

--- home/shells/default.nix
+++ home/shells/default.nix
@@ -3,46 +3,48 @@
     direnv
   ];
 
-  programs.bash = {
-    enable = true;
-    bashrcExtra = ''
-      eval "$(direnv hook bash)"
-    '';
-  };
+  programs = {
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
 
-  programs.zsh = {
-    enable = true;
-
-    initExtra = ''
-      eval "$(direnv hook zsh)"
-    '';
-
-    localVariables = {
-      ZSH_AUTOSUGGEST_STRATEGY = [
-        "match_prev_cmd"
-        "completion"
-      ];
+      enableBashIntegration = true;
+      enableZshIntegration = true;
     };
 
-    plugins = [
-      {
-        name = "zsh-autosuggestions";
-        src = pkgs.fetchFromGitHub {
-          owner = "zsh-users";
-          repo = "zsh-autosuggestions";
-          rev = "v0.7.0";
-          hash = "sha256-KLUYpUu4DHRumQZ3w59m9aTW6TBKMCXl2UcKi4uMd7w=";
-        };
-      }
-      {
-        name = "zsh-syntax-highlighting";
-        src = pkgs.fetchFromGitHub {
-          owner = "zsh-users";
-          repo = "zsh-syntax-highlighting";
-          rev = "0.8.0";
-          hash = "sha256-iJdWopZwHpSyYl5/FQXEW7gl/SrKaYDEtTH9cGP7iPo=";
-        };
-      }
-    ];
+    bash.enable = true;
+
+    zsh = {
+      enable = true;
+
+      localVariables = {
+        ZSH_AUTOSUGGEST_STRATEGY = [
+          "match_prev_cmd"
+          "completion"
+        ];
+      };
+
+      plugins = [
+        {
+          name = "zsh-autosuggestions";
+          src = pkgs.fetchFromGitHub {
+            owner = "zsh-users";
+            repo = "zsh-autosuggestions";
+            rev = "v0.7.0";
+            hash = "sha256-KLUYpUu4DHRumQZ3w59m9aTW6TBKMCXl2UcKi4uMd7w=";
+          };
+        }
+        {
+          name = "zsh-syntax-highlighting";
+          src = pkgs.fetchFromGitHub {
+            owner = "zsh-users";
+            repo = "zsh-syntax-highlighting";
+            rev = "0.8.0";
+            hash = "sha256-iJdWopZwHpSyYl5/FQXEW7gl/SrKaYDEtTH9cGP7iPo=";
+          };
+        }
+      ];
+    };
   };
+
 }
